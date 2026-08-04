4350689988babf45156f5b800e66048ef9108da8
Sterevision <fukkyousuka@gmail.com>
2026-08-04 17:16:59 +0500

    fix: опечатка в адресе тайлов карты (rastertile -> rastertiles)

--- static/js/map.js
+++ static/js/map.js
@@ -120,7 +120,7 @@
             zoomControl: false
         }).setView([55.75, 37.62], 13);
 
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertile/voyager/{z}/{x}/{y}{r}.png', {
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
             maxZoom: 19
         }).addTo(map);
 
